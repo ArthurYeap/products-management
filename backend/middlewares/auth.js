116445e0f86669c65f88228d3d6be9cc85eca84f
Yeap Xian <pixelwolf558@gmail,com>
2026-09-21 13:43:51 +0800

    setup part1

---
+++ backend/middlewares/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+
+exports.authenticate = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        console.log(process.env.JWT_SECRET_KEY)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const user = await User.findOne({ email: decoded.userEmail });
+        if (!user) throw new Error("No user found!");
+        req.user = user;
+        next();
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+}
