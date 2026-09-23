09086b81ce13be06602625d6be05f56444195420
Yeap Xian <pixelwolf558@gmail,com>
2026-09-23 10:41:15 +0800

    finish part 3

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,7 +4,6 @@ const User = require('../models/User')
 exports.authenticate = async (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
-        console.log(process.env.JWT_SECRET_KEY)
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
         const user = await User.findOne({ email: decoded.userEmail });
         if (!user) throw new Error("No user found!");
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState , useEffect} from 'react';
 import './Login.css';
-import axios from 'axios';
+import api from "../utils/api";
 import {Link} from "react-router-dom";
 import {useNavigate} from "react-router-dom";
 
@@ -19,7 +19,7 @@ function Login() {
         // 💡 Add your login / authentication logic here
         console.log('Form submitted:', { email, password });
         try {
-            const response = await axios.post("http://localhost:3000/users/login", {
+            const response = await api.post("/users/login", {
                 email,
                 password
             })
